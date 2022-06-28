df9cf45f0063414bcc2e0bac4e4285e4b95851d3
suguna-singh <sugunasingh528@gmail.com>
2022-06-28 15:59:16 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,10 @@ export class AppComponent implements OnInit {
     this.helperService
       .getUser(this.form.get('name').value)
       .subscribe((resp: any) => {
-        this.userList = resp.filter(item=>item.fork==this.form.get('fork').value).sort((a, b) => b.size - a.size);
+        if(!this.form.get('fork').value){
+          resp = resp.filter(item=>item.fork==this.form.get('fork').value)
+        }
+        this.userList = resp.sort((a, b) => b.size - a.size);
         console.log(resp);
       },(err)=>{
 console.log("err",err)
